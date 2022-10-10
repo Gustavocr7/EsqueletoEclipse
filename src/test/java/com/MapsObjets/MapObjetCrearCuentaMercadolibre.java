7f2bd84cc4bd30574e302b5843a3b4eaf5e08324
Gustavo <g.mora398@gmail.com>
2022-10-10 16:55:10 -0500

    espacios en MapObjet

--- src/test/java/com/MapsObjets/MapObjetCrearCuentaMercadolibre.java
+++ src/test/java/com/MapsObjets/MapObjetCrearCuentaMercadolibre.java
@@ -14,6 +14,9 @@ public class MapObjetCrearCuentaMercadolibre extends ClaseBase {
 	
 	protected By linkCrearcuenta=By.xpath("//nav/a[text()='Crea tu cuenta']");
 	
+	
+	
+	
 	protected By btnCookie=By.xpath("//div[@class='cookie-consent-banner-opt-out__actions']/button[text()='Entendido']");
 	protected By btnCrearcuenta=By.xpath("//span[text()='Crear cuenta']");
 	protected By btnCheckBox=By.id("terms-and-conds");
